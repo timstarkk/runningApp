815d8d482022a60359002fc4af7c8057cc394534
timstarkk <tim@timstark.dev>
2020-07-06 02:33:24 -0500

    change title to friends

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Run</Text>
+      <Text style={styles.title}>Friends</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <FriendsScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
