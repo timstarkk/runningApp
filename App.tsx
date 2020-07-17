2bbea08c2e9544e978be5a65066243b06c5f70ce
timstarkk <tim@timstark.dev>
2020-07-17 18:37:11 -0500

    comment out pubnub code

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import PubNub from 'pubnub';
+// import PubNub from 'pubnub';
 import { PubNubProvider, PubNubConsumer } from 'pubnub-react';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-const pubnub = new PubNub({
-  publishKey: 'pub-c-9c5f94fa-34a9-4c8c-be43-576ece2df432',
-  subscribeKey: 'sub-c-c7c43b00-c0db-11ea-a57f-4e41fc185ce6',
-});
+// const pubnub = new PubNub({
+//   publishKey: 'pub-c-9c5f94fa-34a9-4c8c-be43-576ece2df432',
+//   subscribeKey: 'sub-c-c7c43b00-c0db-11ea-a57f-4e41fc185ce6',
+// });
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
