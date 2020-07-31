6b52e68124520835ea500816ae255a7649bf191b
timstarkk <tim@timstark.dev>
2020-07-31 00:52:06 -0500

    import AccountTabNavigator

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,6 +8,7 @@ import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 import AccountScreen from '../screens/AccountScreen';
+import { AccountTabParamList } from '../types';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -30,7 +31,21 @@ function RootNavigator() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Screen name="Account" component={AccountScreen} options={{ headerTitle: 'Character', headerLeft: () => <FontAwesome5 size={30} style={{ marginBottom: -3, marginLeft: 15 }} color="grey" name="user-circle"/>}} />
+      <Stack.Screen name="Account" component={AccountTabNavigator} options={{ headerTitle: 'Character' }} />
     </Stack.Navigator>
   );
+}
+
+const AccountTabStack = createStackNavigator<AccountTabParamList>();
+
+function AccountTabNavigator() {
+  return (
+    <AccountTabStack.Navigator>
+      <AccountTabStack.Screen 
+        name="AccountTabScreen"
+        component={AccountScreen}
+        options={{ headerTitle: 'Character' }}
+      />
+    </AccountTabStack.Navigator>
+  )
 }
