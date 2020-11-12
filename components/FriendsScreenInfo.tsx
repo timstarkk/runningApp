6acad8ded07ca410afe4fb87744eaf8919fbe7ae
Tim Stark <timstarkk@icloud.com>
2020-11-12 03:10:30 -0600

    remove comments to clean up code

--- components/FriendsScreenInfo.tsx
+++ components/FriendsScreenInfo.tsx
@@ -10,12 +10,6 @@ export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        {/* <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text> */}
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
@@ -24,21 +18,8 @@ export default function EditScreenInfo({ path }: { path: string }) {
           <MonoText>{'Simple friends screen with an add friend button at the top right, within the kind of header, and underneath in rows each friend based on most recent conversation, you can chat, or view the friends profile by clicking on their avatar'}</MonoText>
         </View>
 
-        {/* <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
-        </Text> */}
       </View>
 
-      {/* <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </TouchableOpacity>
-      </View> */}
     </View>
   );
 }
